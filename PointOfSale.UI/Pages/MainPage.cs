691dad9baacd118342a7c9dd4a1569f7245b2bae
Will Smith <william.smith12@imperial.ac.uk>
2013-01-22 02:00:50 +0000

    Implemented Demo for ActionSelectPanel

--- PointOfSale.UI/Pages/MainPage.cs
+++ PointOfSale.UI/Pages/MainPage.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PointOfSale.UI.Models;
 
 namespace PointOfSale.UI.Pages
 {
@@ -15,6 +16,39 @@ namespace PointOfSale.UI.Pages
         public MainPage()
         {
             InitializeComponent();
+
+            Load += MainPage_Load;
+        }
+
+        void MainPage_Load(object sender, EventArgs e)
+        {
+            itemPanel.Rows = 5;
+            itemPanel.Columns = 2;
+
+            for (var i = 0; i < 25; i++)
+            {
+                var locali = i;
+
+                itemPanel.Actions.Add(new PredicateInputAction
+                {
+                    Name = "Item " + i,
+                    Predicate = () => MessageBox.Show("You clicked item " + locali)
+                });
+            }
+            
+            categoryPanel.Rows = 8;
+            categoryPanel.Columns = 1;
+
+            for (var i = 0; i < 30; i++)
+            {
+                var locali = i;
+
+                categoryPanel.Actions.Add(new PredicateInputAction
+                {
+                    Name = "Category " + i,
+                    Predicate = () => MessageBox.Show("You clicked category " + locali)
+                });
+            }
         }
     }
 }
