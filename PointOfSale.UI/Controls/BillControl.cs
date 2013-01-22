1a3ae46a5d6c17bfd474ef8142eca068ef10973b
Tzabar1994 <daniel@tzabar.org>
2013-01-22 18:00:27 +0000

    I think All I did was Change a Button Message

--- PointOfSale.UI/Controls/BillControl.cs
+++ PointOfSale.UI/Controls/BillControl.cs
@@ -16,5 +16,10 @@ namespace PointOfSale.UI.Controls
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
