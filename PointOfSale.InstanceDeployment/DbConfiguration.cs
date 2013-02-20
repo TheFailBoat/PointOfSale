20a4b620993e9768cbc636cfd816f3c25dca2f04
James Phillips <james@thefailboat.co.uk>
2013-02-20 23:51:48 +0000

    InstanceDeployment console app skeleton (for command line deployments and graphical through a future NSIS installer).

---
+++ PointOfSale.InstanceDeployment/DbConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace PointOfSale.InstanceDeployment
+{
+    public class DbConfiguration
+    {
+        public string Server { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Database { get; set; }
+
+        public bool UseWindowsAuth { get; set; }
+
+        public DbConfiguration()
+        {
+            Server = "LOCALHOST\\SQLEXPRESS";
+            UseWindowsAuth = false;
+        }
+
+        public string GetConnectionString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            SqlConnectionStringBuilder.AppendKeyValuePair(builder, "Server", Server);
+            SqlConnectionStringBuilder.AppendKeyValuePair(builder, "Database", Database);
+
+            if (!UseWindowsAuth)
+            {
+                SqlConnectionStringBuilder.AppendKeyValuePair(builder, "User Id", Username);
+                SqlConnectionStringBuilder.AppendKeyValuePair(builder, "Password", Password);
+            }
+            else
+            {
+                SqlConnectionStringBuilder.AppendKeyValuePair(builder, "Trusted_Connection", "True");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ PointOfSale.InstanceDeployment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PointOfSale.Logic.Data;
+
+namespace PointOfSale.InstanceDeployment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbConfiguration dbConfig = new DbConfiguration();
+
+            //TODO: Sane arguments (-dbuser=foo, -DataContext=nh etc). For now we will use a pre-set configuration
+            foreach (string arg in args)
+            {
+                switch (arg)
+                {
+                    
+                }
+            }
+            
+        }
+
+        public void InitialiseDb(DbConfiguration config)
+        {
+            Console.WriteLine("Connecting to the Database...");
+
+            SqlConnection connection = new SqlConnection(config.GetConnectionString());
+
+            EfDataContext efContext = new EfDataContext(connection);
+
+            //Do stuff here...
+        }
+    }
+}
