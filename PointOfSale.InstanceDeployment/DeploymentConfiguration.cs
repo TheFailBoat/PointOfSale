a8c05979e96c4f76fb64948e351265c791bf5e1b
James Phillips <james@thefailboat.co.uk>
2013-02-22 23:36:01 +0000

    InstanceDeployment Work

---
+++ PointOfSale.InstanceDeployment/DeploymentConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.InstanceDeployment
+{
+    class DeploymentConfiguration
+    {
+        public bool CreateAdmin { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
--- PointOfSale.InstanceDeployment/Program.cs
+++ PointOfSale.InstanceDeployment/Program.cs
@@ -14,12 +14,14 @@ namespace PointOfSale.InstanceDeployment
         static void Main(string[] args)
         {
             DbConfiguration dbConfig = new DbConfiguration();
+            DeploymentConfiguration deployConfig = new DeploymentConfiguration();
 
             //TODO: Sane arguments (-dbuser=foo, -DataContext=nh etc). For now we will use a pre-set configuration
-            if (args.Count() < 1) PrintHelp();
+            if (args.Count() < 1) Exit(0, PrintHelp());
             foreach (string a in args)
             {
                 string[] arg = a.Split(new char['='], 2);
+                arg[0] = arg[0].Substring(1); //remove '-'
                 switch (arg[0].ToLower())
                 {
                     case "server":
@@ -56,6 +58,16 @@ namespace PointOfSale.InstanceDeployment
                         }
                         if (arg.Count() < 2) Exit(0, "You cannot use both Trusted and SQL Authentication!");
                         break;
+                    case "createadmin":
+                        deployConfig.CreateAdmin = true;
+                        break;
+                    case "adminpassword":
+                        if (arg.Count() < 2) Exit(0, "You must provide a password!");
+                        deployConfig.AdminPassword = arg[1];
+                        break;
+                    case "shell":
+                        Exit(0, "WIP!!");
+                        break;
                     default:
                         //invalid argument
                         Console.WriteLine("The argument " + arg + " is invalid.");
@@ -77,46 +89,58 @@ namespace PointOfSale.InstanceDeployment
             Exit();
         }
 
-        public void InitialiseDb(DbConfiguration config)
+        public void InitialiseDb(DbConfiguration dbConfig, DeploymentConfiguration deployConfig)
         {
             Console.WriteLine("Connecting to the Database...");
 
-            SqlConnection connection = new SqlConnection(config.GetConnectionString());
+            SqlConnection connection = new SqlConnection(dbConfig.GetConnectionString());
             try
             {
-                connection.Open();
-                Console.WriteLine("Test Connection suceeded");
+                EfDataContext efContext = new EfDataContext(connection);
+                if (deployConfig.CreateAdmin)
+                {
+                    if (string.IsNullOrWhiteSpace(deployConfig.AdminPassword))
+                    {
+                        Console.WriteLine("An Administrator password was not specified.");
+                        Console.WriteLine("Skipping admin creation because of this.");
+                    }
+                    else
+                    {
+                        //create an admin user
+                    }
+                }
             }
             catch(SqlException se)
             {
-                Exit(1, "A problem occured connecting to the database: " + se.Message);
-            }
-            finally
-            {
-                connection.Close();
+                Exit(1, "A problem occured whilst querying the Database:\n" + se.Message);
             }
 
-            EfDataContext efContext = new EfDataContext(connection);
-            
             //more stuff here
         }
 
-        static void PrintHelp()
+        static string PrintHelp()
         {
-            Console.WriteLine("");
-            Console.WriteLine("TFB PointOfSale Instance Deployment Manager");
-            Console.WriteLine("");
-            Console.WriteLine("Reccomended Usage: posDeploy.exe -server=localhost -db=tfbpos -trustedConnection");
-            Console.WriteLine("");
-            Console.WriteLine("Deploys new PointOfSale databases on MSSQL Server");
-            Console.WriteLine("");
-            Console.WriteLine("Command Line Arguments:");
-            Console.WriteLine("-Server=localhost\\SQLEXPRESS - The MSSQL Server to connect to.");
-            Console.WriteLine("-Database=PointOfSale - The database to use on the SQL Server.");
-            Console.WriteLine("-Username=sa - The SQL User to connect with (Cannot be used in a Trusted Connection.)");
-            Console.WriteLine("-Password=letmein - The Password to authenticate with (Cannot be used in a Trusted Connection.)");
-            Console.WriteLine("-TrustedConnection - Use Windows Credentials (Reccomended. Cannot be used with SQL authentication.)");
-
+            string helpText = "TFB PointOfSale Instance Deployment Manager\n";
+            helpText += "Reccomended Usage: posDeploy.exe -server=localhost -db=tfbpos -trustedConnection\n";
+            helpText += "\n";
+            helpText += "Deploys new PointOfSale databases on MSSQL Server.\n";
+            helpText += "By default no action will take place after a connection is established.\n";
+            helpText += "This may be desirable, as the Database structure will be created anyway\n";
+            helpText += "\n";
+            helpText += "Command Line Arguments for Database Connection:\n";
+            helpText += "-Server=localhost\\SQLEXPRESS - The MSSQL Server to connect to.\n";
+            helpText += "-Database=PointOfSale - The database to use on the SQL Server.\n";
+            helpText += "-Username=sa - The SQL User to connect with (SQL Authentication)\n";
+            helpText += "-Password=letmein - The Password to authenticate with (SQL Authentication)\n";
+            helpText += "-TrustedConnection - Use Windows Credentials (More secure)\n";
+            helpText += "\n";
+            helpText += "Command Line Arguments for Deployment:\n";
+            helpText += "-CreateAdmin - Creates an Admin user. -AdminPassword sets password.\n";
+            helpText += "-AdminPassword=password - The password for the POS \"Admin\" User.\n";
+            helpText += "\n";
+            helpText += "A PointOfSale Management Shell can be opened by using the -shell argument\n";
+            helpText += "The database must already be deployed and you will be asked to Login as Admin.\n";
+            return helpText;
         }
 
         static void Exit(int errorCode, string error)
